4c76f9a36d9d89a07d12115828e5d395cd44d3ca
dmeshkovGit <140300183+dmeshkovGit@users.noreply.github.com>
2023-11-03 11:42:07 +0200

    HW done

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,23 @@
-function slugify(title) {
-    const slugWords = title.toLowerCase().split(" ") 
-    return slugWords.join("-")
+function isEnoughCapacity(products, containerSize) {
+    let prodamount = 0
+for (const product of Object.values(products)) {
+    prodamount += product;
+}
+    return prodamount <= containerSize;
 }
 
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
